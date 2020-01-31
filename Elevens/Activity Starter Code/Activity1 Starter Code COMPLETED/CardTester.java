6065192e7f6ef785087b6ceee47ac6112b492348
795294 <795294@seq.org>
2020-01-31 10:27:30 -0800

    elevens

---
+++ Elevens/Activity Starter Code/Activity1 Starter Code COMPLETED/CardTester.java
@@ -0,0 +1,16 @@
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+    /**
+     * The main method in this class checks the Card operations for consistency.
+     *  @param args is not used.
+     */
+    public static void main(String[] args) {
+        Card card1 = new Card ("ten", "spades", 10);
+        
+        System.out.println(card1.toString());
+        
+    }
+}
